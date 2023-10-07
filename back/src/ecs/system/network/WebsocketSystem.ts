c3b572852d2d31b6d9974c6cf8be8bb39e79a8dc
ercan <ercanfr@protonmail.com>
2023-10-08 02:15:44 +0300

    Movement system refactored

--- back/src/ecs/system/network/WebsocketSystem.ts
+++ back/src/ecs/system/network/WebsocketSystem.ts
@@ -12,6 +12,7 @@ import { PhysicsBodyComponent } from "../../component/PhysicsBodyComponent.js";
 import { WebSocketComponent } from "../../component/WebsocketComponent.js";
 import { EntityManager } from "../../../../../shared/entity/EntityManager.js";
 import { Player } from "../../entity/Player.js";
+import { InputProcessingSystem } from "../InputProcessingSystem.js";
 
 type MessageHandler = (ws: any, message: any) => void;
 
@@ -19,6 +20,7 @@ export class WebsocketSystem {
   private port = 8001;
   private players: Player[] = [];
   private messageHandlers: Map<ClientMessageType, MessageHandler> = new Map();
+  private inputProcessingSystem: InputProcessingSystem;
 
   constructor() {
     const app = App();
@@ -49,56 +51,24 @@ export class WebsocketSystem {
       }
     });
 
+    this.inputProcessingSystem = new InputProcessingSystem();
+
     // TODO: Use MovementSystem (ECS approach)
     this.addMessageHandler(ClientMessageType.INPUT, async (ws, message) => {
       const inputMessage = message as InputMessage;
 
       // Access 'ws' if needed within the handler
       const player = this.findPlayer(ws);
+
       if (!player) {
-        console.error("Can't find player with that input message.");
+        console.error(`Entity with WS ${ws} not found.`);
         return;
       }
 
-      const physicsBodyComponent = player
-        .getEntity()
-        .getComponent(PhysicsBodyComponent);
-
-      if (!physicsBodyComponent) {
-        console.error("Player doesn't have a rigidbody -> can't apply input");
-        return;
-      }
-
-      // Define the impulse values for each direction
-      const impulse = new Rapier.Vector3(0, 0, 0);
-      const speed = 100;
-      // Handle input for moving up
-      if (inputMessage.up) {
-        impulse.z = -speed; // Adjust the vertical impulse value as needed (e.g., for jumping)
-      }
-
-      // Handle input for moving down (e.g., crouching)
-      if (inputMessage.down) {
-        impulse.z = speed; // Adjust the vertical impulse value as needed
-      }
-
-      // Handle input for moving left
-      if (inputMessage.left) {
-        impulse.x = -speed; // Adjust the horizontal impulse value as needed (e.g., for moving left)
-      }
-
-      // Handle input for moving right
-      if (inputMessage.right) {
-        impulse.x = speed; // Adjust the horizontal impulse value as needed (e.g., for moving right)
-      }
-
-      if (inputMessage.space) {
-        impulse.y = speed;
-      }
-
-      // Apply the accumulated impulse to the physics body
-      // physicsBodyComponent.body.applyImpulse(impulse, false);
-      physicsBodyComponent.body.setLinvel(impulse, true);
+      this.inputProcessingSystem.receiveInputPacket(
+        player.getEntity(),
+        inputMessage
+      );
     });
   }
 
