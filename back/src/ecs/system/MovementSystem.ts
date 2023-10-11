a26a0db361d37201ae10e6b3e70fc151203e93ec
ercan <ercanfr@protonmail.com>
2023-10-11 23:49:20 +0300

    Kinematic cubes

--- back/src/ecs/system/MovementSystem.ts
+++ back/src/ecs/system/MovementSystem.ts
@@ -2,16 +2,25 @@ import { InputComponent } from "../component/InputComponent.js";
 import { PhysicsBodyComponent } from "../component/PhysicsBodyComponent.js";
 import { Entity } from "../../../../shared/entity/Entity.js";
 import Rapier from "../../physics/rapier.js";
+import { PositionComponent } from "../../../../shared/component/PositionComponent.js";
+import { PhysicsColliderComponent } from "../component/PhysicsColliderComponent.js";
 
 export class MovementSystem {
-  update(entities: Entity[]): void {
+  checkGround() {}
+
+  update(entities: Entity[], world: Rapier.World): void {
     entities.forEach((entity) => {
-      const input = entity.getComponent<InputComponent>(InputComponent);
+      const inputComponent = entity.getComponent(InputComponent);
+      const rigidBodyComponent = entity.getComponent(PhysicsBodyComponent);
+      const colliderComponent = entity.getComponent(PhysicsColliderComponent);
+      const positionComponent = entity.getComponent(PositionComponent);
 
-      const rigidBody =
-        entity.getComponent<PhysicsBodyComponent>(PhysicsBodyComponent);
-
-      if (input && rigidBody) {
+      if (
+        inputComponent &&
+        rigidBodyComponent &&
+        positionComponent &&
+        colliderComponent
+      ) {
         const physicsBodyComponent = entity.getComponent(PhysicsBodyComponent);
         if (!physicsBodyComponent) {
           console.error("Player doesn't have a rigidbody -> can't apply input");
@@ -23,29 +32,52 @@ export class MovementSystem {
         // Define the impulse values for each direction
         const impulse = new Rapier.Vector3(
           0,
-          currentLinVel.y, // Preserve the current Y velocity
+          currentLinVel.y - 0.1, // Preserve the current Y velocity
           0
         );
         const speed = 100 / 2;
 
         // Handle input for moving up
-        if (input.up) {
+        if (inputComponent.up) {
           impulse.z = -speed; // Adjust the vertical impulse value as needed (e.g., for jumping)
         }
         // Handle input for moving down (e.g., crouching)
-        if (input.down) {
+        if (inputComponent.down) {
           impulse.z = speed; // Adjust the vertical impulse value as needed
         }
         // Handle input for moving left
-        if (input.left) {
+        if (inputComponent.left) {
           impulse.x = -speed; // Adjust the horizontal impulse value as needed (e.g., for moving left)
         }
         // Handle input for moving right
-        if (input.right) {
+        if (inputComponent.right) {
           impulse.x = speed; // Adjust the horizontal impulse value as needed (e.g., for moving right)
         }
-        if (input.space) {
-          impulse.y = 50;
+        if (inputComponent.space) {
+          let ray = new Rapier.Ray(
+            {
+              x: positionComponent.x,
+              y: positionComponent.y - 1,
+              z: positionComponent.z,
+            },
+            {
+              x: 0,
+              y: -1,
+              z: 0,
+            }
+          );
+          let hit = world.castRay(
+            ray,
+            1,
+            false,
+            undefined,
+            undefined,
+            colliderComponent.collider
+          );
+          console.log(hit);
+          if (hit != null) {
+            impulse.y = 70;
+          }
         }
         // Apply the accumulated impulse to the physics body
         physicsBodyComponent.body.setLinvel(impulse, true);
