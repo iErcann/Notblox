8c9928643bccc2e75e72f6d8e7410bd0e512981e
ercan <ercanfr@protonmail.com>
2023-10-13 00:28:58 +0300

    Dirty first packet sent with whole snapshot

--- back/src/ecs/component/WebsocketComponent.ts
+++ back/src/ecs/component/WebsocketComponent.ts
@@ -3,7 +3,11 @@
 import { Component } from "../../../../shared/component/Component.js";
 
 export class WebSocketComponent extends Component {
-  constructor(entityId: number, public ws: any) {
+  constructor(
+    entityId: number,
+    public ws: any,
+    public isFirstSnapshotSent = false
+  ) {
     super(entityId);
   }
 }
