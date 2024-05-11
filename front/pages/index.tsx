b49850c6f770ea634ee0ebd01389d7b8bc0e1e39
ercan <ercanfr@protonmail.com>
2024-05-11 17:07:13 +0200

    Changed layout + Domain name + Basic seo

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,41 +1,88 @@
 import KeyboardLayout from "@/components/KeyboardLayout";
 import { ExternalLink, Play } from "lucide-react";
+import { NextSeo } from "next-seo";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container md:w-1/4 mx-auto px-4 h-screen flex flex-col justify-center items-center text-center space-y-4 max-w-screen-lg">
-      <h1 className="text-5xl font-bold mb-4">NoNameYet.io</h1>
-      <Image
-        src="/Logo.png"
-        alt={"Logo"}
-        width={200}
-        height={300}
-        className="rounded-lg shadow-xl"
+    <div className="container md:w-1/4 flex flex-col items-start md:items-center justify-center flex-1  text-center">
+      <NextSeo
+        title="NotBlox Online - Play multiplayer games in your browser"
+        description="Play multiplayer games like Roblox in your browser. Create your own games and share them with your friends."
+        canonical="hhttps://notblox.online/"
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "/Logo.png",
+              width: 800,
+              height: 600,
+              alt: "NotBlox Logo",
+              type: "image/png",
+            },
+          ],
+          siteName: "NotBlox Online",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
       />
 
-      <Link href={"/test"}>
-        <button className="w-full  inline-flex h-14 items-center justify-center rounded-md bg-zinc-900 px-10 text-lg font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300">
-          <Play className="mr-2" />
-          Play Now
-        </button>
-      </Link>
+      <main className="space-y-4 my-8 p-8">
+        <h1 className="text-6xl font-bold">NotBlox.online</h1>
+        <p className="text-2xl">Play multiplayer games in your browser</p>
 
-      <KeyboardLayout />
-      <div className="hidden lg:block">
+        <div className="flex flex-wrap items-center justify-around max-w-4xl sm:w-full space-y-8 ">
+          <Image
+            src="/Logo-removebg-preview.png"
+            alt={"Logo"}
+            width={200}
+            height={300}
+          />
+
+          <Link
+            href={"/test"}
+            className="flex items-center justify-center w-full px-8 py-3 font-medium text-white bg-black border border-transparent rounded-md hover:bg-gray-700 md:py-4 md:text-lg md:px-10"
+          >
+            <Play className="mr-2" />
+            Play Now
+          </Link>
+        </div>
+
+        <KeyboardLayout />
+
         <p className="text-lg font-semibold">What's this?</p>
         <p className="text-sm text-gray-600 my-4">
           This is my playground for learning new technologies and having fun. I
           might get inspired by Roblox (dont sue pls)
         </p>
-        <Link href={"https://discord.gg/kPhgtj49U2"}>
-          <button className="inline-flex h-14 items-center justify-center rounded-md bg-blue-600 px-10 text-lg font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300">
-            <ExternalLink className="mr-2" />
-            Join Discord
-          </button>
+        <Link
+          href={"https://discord.gg/kPhgtj49U2"}
+          className="flex py-2 items-center justify-center w-full px-8   font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700   md:text-lg md:px-10"
+        >
+          <ExternalLink className="mr-2" />
+          Project Discord
         </Link>
-      </div>
+        <Link
+          href={"https://twitter.com/iErcan_"}
+          className="flex py-2 items-center justify-center w-full px-8   font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700   md:text-lg md:px-10"
+        >
+          <ExternalLink className="mr-2" />
+          Twitter
+        </Link>
+        <Link
+          href={"https://github.com/iErcann/NotRoblox"}
+          className="flex py-2 items-center justify-center w-full px-8   font-medium text-white bg-gray-800 border border-transparent rounded-md hover:bg-gray-700   md:text-lg md:px-10"
+        >
+          <ExternalLink className="mr-2" />
+          Source Code
+        </Link>
+      </main>
     </div>
   );
 }
