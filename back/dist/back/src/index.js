2d15ddae3a46714ca8969b889369d9cf709958fd
DigitalDemonix <165849146+DigitalDemonix@users.noreply.github.com>
2024-08-07 02:44:23 -0400

    Add files via upload

--- back/dist/back/src/index.js
+++ back/dist/back/src/index.js
@@ -29,7 +29,7 @@ import { PlayerComponent } from './ecs/component/tag/TagPlayerComponent.js';
 import { ZombieSystem } from './ecs/system/ZombieSystem.js';
 import Rapier from './physics/rapier.js';
 // Initialize the physics world
-const gravity = { x: 0.0, y: -100.0, z: 0.0 };
+const gravity = { x: 0.0, y: -70.0, z: 0.0 };
 const world = new Rapier.World(gravity);
 // Physics systems
 const kinematicPhysicsBodySystem = new KinematicRigidBodySystem();
