bd6ec011481be803756ceecacc3e785f72b5fb96
ercan <ercan@example.com>
2025-01-17 00:51:57 +0100

    Fix initial load infinite lerp, set transform components directly if available

--- front/game/ecs/system/MeshSystem.ts
+++ front/game/ecs/system/MeshSystem.ts
@@ -5,43 +5,70 @@ import { MeshComponent } from '../component/MeshComponent.js'
 import { ComponentAddedEvent } from '@shared/component/events/ComponentAddedEvent.js'
 import { ComponentRemovedEvent } from '@shared/component/events/ComponentRemovedEvent.js'
 import * as THREE from 'three'
+import { PositionComponent } from '@shared/component/PositionComponent.js'
+import { RotationComponent } from '@shared/component/RotationComponent.js'
+import { EntityManager } from '@shared/system/EntityManager.js'
 
 export class MeshSystem {
   update(entities: Entity[], renderer: Renderer) {
-    // Handle added components
-    const addedMeshEvents = EventSystem.getEventsWrapped(ComponentAddedEvent, MeshComponent)
-    for (const addedEvent of addedMeshEvents) {
-      const meshComponent = addedEvent.component as MeshComponent
-      if (meshComponent) {
-        console.log('MeshSystem: Adding mesh to scene')
-        this.activateShadows(meshComponent)
-        renderer.scene.add(meshComponent.mesh)
-      }
-    }
+    this.handleAddedMeshes(entities, renderer)
+    this.handleRemovedMeshes(renderer)
+  }
 
-    // Handle removed components
-    const removedMeshEvents = EventSystem.getEventsWrapped(ComponentRemovedEvent, MeshComponent)
-    for (const removedEvent of removedMeshEvents) {
-      const meshComponent = removedEvent.component as MeshComponent
-      if (meshComponent) {
-        renderer.scene.remove(meshComponent.mesh)
+  private handleAddedMeshes(entities: Entity[], renderer: Renderer) {
+    const addedMeshEvents = EventSystem.getEventsWrapped(ComponentAddedEvent, MeshComponent)
+
+    for (const addedEvent of addedMeshEvents) {
+      const entity = EntityManager.getEntityById(entities, addedEvent.entityId)
+      if (!entity) {
+        console.error('MeshSystem: Entity not found')
+        continue
       }
+
+      const meshComponent = addedEvent.component
+      this.updateMeshTransform(entity, meshComponent)
+      this.addMeshToScene(meshComponent, renderer)
     }
   }
-  activateShadows(meshComponent: MeshComponent) {
-    if (meshComponent) {
-      const object3D = meshComponent.mesh
-      object3D.castShadow = true // Make the mesh cast shadows
-      object3D.receiveShadow = true // Make the mesh receive shadows
 
-      // Enable shadows for all child meshes
-      object3D.traverse((child) => {
-        if (child instanceof THREE.Mesh) {
-          child.material.metalness = 0
-          child.castShadow = true // Make the child mesh cast shadows
-          child.receiveShadow = true // Make the child mesh receive shadows
-        }
-      })
+  private handleRemovedMeshes(renderer: Renderer) {
+    const removedMeshEvents = EventSystem.getEventsWrapped(ComponentRemovedEvent, MeshComponent)
+
+    for (const removedEvent of removedMeshEvents) {
+      const meshComponent = removedEvent.component
+      renderer.scene.remove(meshComponent.mesh)
     }
+  }
+
+  private updateMeshTransform(entity: Entity, meshComponent: MeshComponent) {
+    const positionComponent = entity.getComponent(PositionComponent)
+    if (positionComponent) {
+      meshComponent.mesh.position.set(positionComponent.x, positionComponent.y, positionComponent.z)
+    }
+
+    const rotationComponent = entity.getComponent(RotationComponent)
+    if (rotationComponent) {
+      meshComponent.mesh.rotation.set(rotationComponent.x, rotationComponent.y, rotationComponent.z)
+    }
+  }
+
+  private addMeshToScene(meshComponent: MeshComponent, renderer: Renderer) {
+    console.log('MeshSystem: Adding mesh to scene')
+    this.activateShadows(meshComponent)
+    renderer.scene.add(meshComponent.mesh)
+  }
+
+  private activateShadows(meshComponent: MeshComponent) {
+    const object3D = meshComponent.mesh
+    object3D.castShadow = true
+    object3D.receiveShadow = true
+
+    object3D.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material.metalness = 0
+        child.castShadow = true
+        child.receiveShadow = true
+      }
+    })
   }
 }
