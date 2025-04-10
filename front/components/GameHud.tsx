bb20f65df2c0760a4e0ef767664377260f00bd81
iErcann <ercanfr@protonmail.com>
2025-04-10 21:21:35 +0200

    style(ui): put Discord link better in the UI

--- front/components/GameHud.tsx
+++ front/components/GameHud.tsx
@@ -143,7 +143,7 @@ export default function GameHud({
         ))}
       </div>
       <div className="flex justify-between items-center">
-        <div className="shadow-4xl p-4  rounded-lg space-y-1 bg-gray-800 bg-opacity-20">
+        <div className="shadow-4xl p-4 rounded-lg space-y-1 bg-gray-800 bg-opacity-20">
           <p className="text-sm">👋 Welcome to </p>
           <a
             className="text-sm md:text-2xl font-bold pointer-events-auto hover:text-gray-400"
@@ -151,25 +151,20 @@ export default function GameHud({
           >
             NotBlox<span className="font-medium">.online</span>
           </a>
-          <div className="space-x-4 text-sm flex justify-around  text-white pointer-events-auto">
+          <div className="text-sm flex justify-center text-white pointer-events-auto">
             <Link
               href="https://discord.gg/kPhgtj49U2"
               target="_blank"
-              className="hover:text-gray-400"
+              className="flex items-center justify-center mt-2 gap-2 px-3 py-1 w-full bg-blue-800 opacity-60 hover:bg-blue-700 rounded-md transition-colors"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 640 512"
-                className="h-6 w-6 text-white fill-white hover:fill-gray-600"
+                className="h-5 w-5 text-white fill-white"
               >
                 <path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
               </svg>
-            </Link>
-            <Link href="https://twitter.com/iErcan_" target="_blank">
-              <Twitter className="h-6 w-6 text-white hover:text-gray-600" />
-            </Link>
-            <Link href="https://github.com/iErcann/Notblox" target="_blank">
-              <Github className="h-6 w-6 text-white hover:text-gray-600" />
+              <span className="font-medium">Join Discord</span>
             </Link>
           </div>
         </div>
